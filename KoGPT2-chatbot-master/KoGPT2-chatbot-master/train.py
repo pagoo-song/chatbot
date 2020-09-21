db9b28a741002f16915d9b3c689f89a1eef24a32
pagoo-song <64295203+pagoo-song@users.noreply.github.com>
2020-09-21 21:03:10 +0900

    Update train.py

--- KoGPT2-chatbot-master/KoGPT2-chatbot-master/train.py
+++ KoGPT2-chatbot-master/KoGPT2-chatbot-master/train.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import argparse
 import logging
 import math
@@ -6,12 +7,12 @@ import gluonnlp as nlp
 import mxnet as mx
 import pandas as pd
 from gluonnlp.data import SentencepieceTokenizer
-from kobert.mxnet_kobert import get_mxnet_kobert_model
-from kobert.utils import get_tokenizer
+from kogpt2.mxnet_kogpt2 import get_mxnet_kogpt2_model
+from kogpt2.utils import get_tokenizer
 from mxnet import gluon, nd
 from mxnet.gluon import nn
 
-parser = argparse.ArgumentParser(description='Simsimi based on KoBERT')
+parser = argparse.ArgumentParser(description='Simsimi based on KoGPT-2')
 
 parser.add_argument('--num-epoch',
                     type=int,
@@ -20,7 +21,7 @@ parser.add_argument('--num-epoch',
 
 parser.add_argument('--max-seq-len',
                     type=int,
-                    default=64,
+                    default=32,
                     help='max sentence length on input (default: 32)')
 
 parser.add_argument('--batch-size',
@@ -41,7 +42,7 @@ parser.add_argument('--sentiment',
 
 parser.add_argument('--model_params',
                     type=str,
-                    default='kobert_chat.params',
+                    default='kogpt2_chat.params',
                     help='model binary for starting chat')
 
 parser.add_argument('--train',
@@ -70,7 +71,7 @@ SENT = '<unused1>'
 
 
 class ChatDataset(gluon.data.Dataset):
-    def __init__(self, chats, tok_path, vocab, max_len=64):
+    def __init__(self, chats, tok_path, vocab, max_len=32):
         self._data = chats
         self._tok_path = tok_path
         self.tokenizer = None
@@ -114,11 +115,16 @@ class ChatDataset(gluon.data.Dataset):
         ]
         a_len = len(a_toked)
         if q_len + a_len > self.max_len:
-            remains = self.max_len - q_len
-            a_len = remains
-            a_toked = a_toked[-a_len:]
-            assert a_len == len(a_toked)
-        # [mask, mask, ...., mask, ..., <bos>,..A.. <eos>, <pad>....]
+            a_len = self.max_len - q_len
+            if a_len <= 0:
+                q_toked = q_toked[-(int(self.max_len/2)):]
+                q_len = len(q_toked)
+                a_len = self.max_len - q_len
+                assert a_len > 0
+            a_toked = a_toked[:a_len]
+            a_len = len(a_toked)
+            assert a_len == len(a_toked), f'{a_len} ==? {len(a_toked)}'
+        # [<mask>, <mask>, ...., <mask>, ..., A.. <eos>, <pad>....]
         labels = [
             self.maskt,
         ] * q_len + a_toked[1:]
@@ -134,14 +140,14 @@ class ChatDataset(gluon.data.Dataset):
                 self.padder(self.vocab[labels]))
 
 
-class KoBERTChat(nn.HybridBlock):
-    def __init__(self, kobert, prefix=None, params=None):
-        super(KoBERTChat, self).__init__(prefix=prefix, params=params)
-        self.kobert = kobert
+class KoGPT2Chat(nn.HybridBlock):
+    def __init__(self, kogpt2, prefix=None, params=None):
+        super(KoGPT2Chat, self).__init__(prefix=prefix, params=params)
+        self.kogpt2 = kogpt2
 
     def hybrid_forward(self, F, inputs):
         # (batch, seq_len, hiddens)
-        output, _ = self.kobert(inputs)
+        output, _ = self.kogpt2(inputs)
         return output
 
 
@@ -153,7 +159,7 @@ else:
 
 def train():
     tok_path = get_tokenizer()
-    model, vocab = get_mxnet_kobert_model(ctx=ctx)
+    model, vocab = get_mxnet_kogpt2_model(ctx=ctx)
     # tok = SentencepieceTokenizer(tok_path, num_best=0, alpha=0)
 
     data = pd.read_csv('Chatbot_data/ChatbotData.csv')
@@ -166,8 +172,8 @@ def train():
                                                 batch_size=batch_size,
                                                 num_workers=5,
                                                 shuffle=True)
-    kobertqa = KoBERTChat(model)
-    kobertqa.hybridize()
+    kogptqa = KoGPT2Chat(model)
+    kogptqa.hybridize()
 
     # softmax cross entropy loss for classification
     loss_function = gluon.loss.SoftmaxCrossEntropyLoss()
@@ -175,16 +181,16 @@ def train():
 
     num_epochs = opt.num_epoch
     lr = 5e-5
-    trainer = gluon.Trainer(kobertqa.collect_params(), 'bertadam', {
+    trainer = gluon.Trainer(kogptqa.collect_params(), 'bertadam', {
         'learning_rate': lr,
         'epsilon': 1e-8,
         'wd': 0.01
     })
     # LayerNorm과 Bias에는 Weight Decay를 적용하지 않는다.
-    for _, v in kobertqa.collect_params('.*beta|.*gamma|.*bias').items():
+    for _, v in kogptqa.collect_params('.*beta|.*gamma|.*bias').items():
         v.wd_mult = 0.0
     params = [
-        p for p in kobertqa.collect_params().values() if p.grad_req != 'null'
+        p for p in kogptqa.collect_params().values() if p.grad_req != 'null'
     ]
     # learning rate warmup
     accumulate = opt.accumulate
@@ -194,7 +200,7 @@ def train():
     warmup_ratio = 0.1
     num_warmup_steps = int(num_train_steps * warmup_ratio)
     step_num = 0
-    all_model_params = kobertqa.collect_params()
+    all_model_params = kogptqa.collect_params()
 
     log_interval = 50
     neg = -1e18
@@ -220,7 +226,7 @@ def train():
                 mask = mask.as_in_context(ctx)
                 label = label.as_in_context(ctx)
                 # forward computation
-                out = kobertqa(token_ids)
+                out = kogptqa(token_ids)
                 masked_out = nd.where(
                     mask.expand_dims(axis=2).repeat(repeats=out.shape[2],
                                                     axis=2), out,
@@ -246,15 +252,15 @@ def train():
                             math.exp(step_loss / log_interval)))
                 step_loss = 0
     logging.info('saving model file to {}'.format(opt.model_params))
-    kobertqa.save_parameters(opt.model_params)
+    kogptqa.save_parameters(opt.model_params)
 
 
 def chat(model_params, sent='0'):
     tok_path = get_tokenizer()
-    model, vocab = get_mxnet_kobert_model(ctx=ctx)
+    model, vocab = get_mxnet_kogpt2_model(ctx=ctx)
     tok = SentencepieceTokenizer(tok_path, num_best=0, alpha=0)
-    kobertqa = KoBERTChat(model)
-    kobertqa.load_parameters(model_params, ctx=ctx)
+    kogptqa = KoGPT2Chat(model)
+    kogptqa.load_parameters(model_params, ctx=ctx)
     sent_tokens = tok(sent)
     while 1:
         q = input('user > ').strip()
@@ -268,7 +274,7 @@ def chat(model_params, sent='0'):
                                     vocab[EOS, SENT] + vocab[sent_tokens] +
                                     vocab[EOS, S_TKN] +
                                     vocab[a_tok]).expand_dims(axis=0)
-            pred = kobertqa(input_ids.as_in_context(ctx))
+            pred = kogptqa(input_ids.as_in_context(ctx))
             gen = vocab.to_tokens(
                 mx.nd.argmax(
                     pred,
